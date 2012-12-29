d85062472258fd36fb380b8a4cf08de15f190a23
Tyler Kennedy <tk@tkte.ch>
2012-12-29 06:12:34 -0500

    Handle the case where the user removes github oauth access from github. Allow updating existing projects. Allow auto-adding public projects to #commits. Don't create github hook if the project already has a github hook. Dont create #commits channel if project already has it.

--- notifico/views/pimport/__init__.py
+++ notifico/views/pimport/__init__.py
@@ -11,23 +11,30 @@ from flask import (
     request
 )
 from flask.ext import wtf
-from github import Github
+from github import Github, GithubException
 
 from notifico import user_required, app
-from notifico.models import AuthToken, Project, Hook
+from notifico.models import AuthToken, Project, Hook, Channel
 
 pimport = Blueprint('pimport', __name__, template_folder='templates')
 
 
 class GithubForm(wtf.Form):
-    projects = wtf.BooleanField('Import Projects', default=True, description=(
-        'Imports your public and private repositories. Private repos'
-        ' will be imported as private and will never store messages.'
+    update_projects = wtf.BooleanField('Update Projects', default=True,
+        description=(
+            'If a project already exists, update it instead of skipping it.'
     ))
     set_hooks = wtf.BooleanField('Set Hooks', default=False, description=(
-        'Sets up each project you select with event hooks, so that they'
-        ' immediately recieve commits.'
+        'Sets up each project you select with Github web hooks, so that they'
+        ' start recieving commits immediately.'
     ))
+    set_commits = wtf.BooleanField('Add to #commits', default=True,
+        description=(
+            'Sets up public projects you select to broadcast to #commits'
+            ' on irc.freenode.net as a default channel (you can always remove'
+            ' it later).'
+        )
+    )
 
 
 @pimport.route('/github', methods=['GET', 'POST'])
@@ -77,14 +84,31 @@ def github():
     # If we leave type as the default ("all") we also get
     # repos they have permission on, which we probably don't want.
     git = Github(access_token.token)
+    # Test to make sure our token is still good...
+    try:
+        git.get_user().login
+    except GithubException as exception:
+        # Nope!
+        if exception.status == 401:
+            # The user almost certainly removed the OAuth token
+            # from their github applications page. Remove the now-obsolete
+            # token and refresh.
+            access_token = AuthToken.query.filter_by(
+                name='github',
+                owner_id=g.user.id
+            ).first()
+            if access_token:
+                g.user.tokens.remove(access_token)
+                g.db.session.delete(access_token)
+                g.db.session.commit()
+            return redirect(request.path)
+
     user_repos = git.get_user().get_repos(type='all')
 
     summary = None
     options_form = GithubForm()
     if options_form.validate_on_submit():
         summary = []
-        # WTForms "forgets" disabled fields. This should /always/ be True.
-        options_form.projects.data = True
 
         for repo in user_repos:
             # User didn't check the box, don't import this project.
@@ -93,50 +117,79 @@ def github():
             if request.form.get(str(repo.id), None) != 'y':
                 continue
 
+            # Make sure this project doesn't already exists.
             p = Project.by_name_and_owner(repo.name, g.user)
-            if p is not None:
+            if p is None:
+                p = Project.new(
+                    repo.name,
+                    public=not repo.private,
+                    website=repo.homepage
+                )
+                p.full_name = '{0}/{1}'.format(g.user.username, p.name)
+                g.user.projects.append(p)
+                g.db.session.add(p)
+                # We need to commit to generate the project.id which is
+                # used for the following steps.
+                g.db.session.commit()
+
                 summary.append((
-                    (
-                        'Skipping {0} as a project with that name already'
-                        ' exists.'
-                    ).format(repo.name),
+                    'Project {0} created.'.format(repo.name),
+                    True
+                ))
+            elif p is not None and not options_form.update_projects.data:
+                summary.append((
+                    'Skipping existing project {0}.'.format(repo.name),
                     False
                 ))
                 continue
-
-            p = Project.new(
-                repo.name,
-                public=not repo.private,
-                website=repo.homepage
-            )
-            p.full_name = '{0}/{1}'.format(g.user.username, p.name)
-            g.user.projects.append(p)
-            g.db.session.add(p)
-
-            summary.append((
-                'Project {0} created.'.format(repo.name),
-                True
-            ))
+            else:
+                summary.append((
+                    'Project {0} updated.'.format(repo.name),
+                    True
+                ))
 
             if options_form.set_hooks.data:
                 # The user wanted us to auto-create web hooks for them.
-                # We need to commit first to generate the project.id.
-                g.db.session.commit()
+                h = Hook.query.filter_by(
+                    project_id=p.id,
+                    service_id=10
+                ).first()
+                if h is None:
+                    # No hooks for github have been setup yet, so go ahead
+                    # and create one.
+                    h = Hook.new(10)
+                    p.hooks.append(h)
+                    g.db.session.add(h)
 
-                # Create the hook on Notifico's side...
-                h = Hook.new(10)
-                p.hooks.append(h)
-                g.db.session.add(h)
+                    repo.create_hook('web', {
+                        'url': url_for(
+                            'projects.hook_recieve',
+                            pid=p.id,
+                            key=h.key,
+                            _external=True
+                        )
+                    })
 
-                # ... then create the hook on Github's side.
-                repo.create_hook('web', {
-                    'url': url_for(
-                        'projects.hook_recieve',
-                        pid=p.id,
-                        key=h.key,
-                        _external=True
+            if options_form.set_commits.data and p.public:
+                # The user wanted us to add their public projects
+                # to #commits@irc.freenode.net for them, but only if it
+                # isn't already there.
+                c = Channel.query.filter_by(
+                    host='irc.freenode.net',
+                    channel='#commits',
+                    project_id=p.id
+                ).first()
+                if c is None:
+                    # It does *not* already exist, so go ahead and add it.
+                    c = Channel.new(
+                        '#commits',
+                        'irc.freenode.net',
+                        6667,
+                        ssl=False,
+                        public=True
                     )
-                })
+                    p.channels.append(c)
+                    g.db.session.add(c)
 
         g.db.session.commit()
 
