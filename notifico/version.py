d4551261e1f179fd4314f313167cbb6a54b6dcdd
Tyler Kennedy <tk@tkte.ch>
2012-12-28 22:59:35 -0500

    Add setup.py and version.py from redo

---
+++ notifico/version.py
@@ -0,0 +1,3 @@
+# -*- coding: utf8 -*-
+__all__ = ('__version__',)
+__version__ = '1.0.0'
---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+"""
+Notifico is my personal open source MIT replacement to the
+now-defunct http://cia.vc service with my own little spin on things.
+"""
+from setuptools import setup, find_packages
+
+
+def get_version():
+    """
+    Load and return the current Notifico version.
+    """
+    local_results = {}
+    execfile('notifico/version.py', {}, local_results)
+    return local_results['__version__']
+
+
+if __name__ == '__main__':
+    setup(
+        name='Notifico',
+        version=get_version(),
+        long_description=__doc__,
+        packages=find_packages(),
+        include_package_data=True,
+        zip_safe=False,
+        install_requires=[
+            'Flask',
+            'Flask-WTF',
+            'Flask-Gravatar',
+            'fabric',
+            'sqlalchemy',
+            'UtopiaIRC',
+            'gevent',
+            'oauth2',
+            'redis'
+        ],
+        dependency_links=[
+            'https://github.com/TkTech/utopia/tarball/master#egg=UtopiaIRC'
+        ]
+    )
