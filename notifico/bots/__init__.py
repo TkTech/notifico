c58411c8b0a76eb08b7983147a43fbe0f37ed82a
Tyler Kennedy <tk@tkte.ch>
2012-12-28 22:19:02 -0500

    Main bot loop no longer uses multiprocessing, making management with supervisord much easier (supervisord cannot track the forks for termination)

--- notifico/bots/__init__.py
+++ notifico/bots/__init__.py
@@ -1,43 +1,27 @@
 # -*- coding: utf8 -*-
 import json
-from Queue import Empty
-from multiprocessing import Process, Queue
 
 import redis
+import gevent
+from utopia import Network
 
+from notifico.bots.manager import BotManager, Channel
+from notifico.bots.bot import BotificoBot
 import notifico.default_config as config
 
 
-def wait_for_message(q):
-    """
-    Wait for a new message to be forwarded.
-    """
+def start_manager():
     r = redis.StrictRedis(
         host=config.REDIS_HOST,
         port=config.REDIS_PORT,
         db=config.REDIS_DB
     )
-
-    while True:
-        _, message = r.blpop('queue_message')
-        q.put(json.loads(message))
-
-
-def start_manager():
-    q = Queue()
-    p = Process(target=wait_for_message, args=(q,))
-    p.start()
-
-    import gevent
-    from utopia import Network
-    from notifico.bots.manager import BotManager, Channel
-    from notifico.bots.bot import BotificoBot
-
     manager = BotManager(BotificoBot)
 
     while True:
-        try:
-            m = q.get(False)
+        result = r.lpop('queue_message')
+        if result:
+            m = json.loads(result)
             if m['type'] == 'message':
                 channel = m['channel']
                 payload = m['payload']
@@ -55,8 +39,6 @@ def start_manager():
                     ),
                     payload['msg']
                 )
-        except Empty:
-            pass
 
         gevent.sleep(0.1)
 
