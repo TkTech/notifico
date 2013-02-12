d19b3b2e74bb8e8b76441c90e75114d7f64ea00d
Tyler Kennedy <tk@tkte.ch>
2013-02-11 22:57:17 -0500

    Added admin view to see all projects

--- notifico/views/admin/__init__.py
+++ notifico/views/admin/__init__.py
@@ -2,11 +2,13 @@ from flask import (
     Blueprint,
     g,
     url_for,
-    redirect
+    redirect,
+    request,
+    render_template
 )
 
-from notifico import user_required
-from notifico.models import Group
+from notifico import user_required, group_required
+from notifico.models import Group, Project
 
 admin = Blueprint('admin', __name__, template_folder='templates')
 
@@ -25,3 +27,24 @@ def admin_make():
     g.user.add_group('admin')
     g.db.session.commit()
     return redirect(url_for('public.landing'))
+
+
+@admin.route('/projects/', defaults={'page': 1})
+@admin.route('/projects/<int:page>')
+@group_required('admin')
+def admin_projects(page=1):
+    per_page = min(int(request.args.get('l', 25)), 100)
+    sort_by = request.args.get('s', 'created')
+
+    q = Project.query.order_by(False)
+    q = q.order_by({
+        'created': Project.created.desc(),
+        'messages': Project.message_count.desc()
+    }.get(sort_by, Project.created.desc()))
+
+    pagination = q.paginate(page, per_page, False)
+
+    return render_template('admin_projects.html',
+        pagination=pagination,
+        per_page=per_page
+    )
