135d6ebe25849a2ca909ec04674de4a78af13377
Tyler Kennedy <tk@tkte.ch>
2013-05-25 17:42:54 -0400

    A bit of whitespace on the landing, two unused stats.

--- notifico/views/public/__init__.py
+++ notifico/views/public/__init__.py
@@ -34,7 +34,6 @@ def landing():
         new_projects=new_projects,
         top_networks=stats.top_networks(limit=10),
         total_networks=stats.total_networks(),
-        total_messages=stats.total_messages(),
         total_users=stats.total_users()
     )
 
