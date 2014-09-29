f5ee5e4f4a00e44942f640a5f20fc953a4b222a6
Tyler Kennedy <tk@tkte.ch>
2014-09-29 10:25:46 -0400

    Add support for colours in the plain text hook. Closes #47

--- notifico/services/hooks/plain.py
+++ notifico/services/hooks/plain.py
@@ -1,7 +1,16 @@
 # -*- coding: utf8 -*-
 __all__ = ('PlainTextHook',)
+from flask.ext import wtf
 
 from notifico.services.hooks import HookService
+
+
+class PlainTextConfigForm(wtf.Form):
+    use_colors = wtf.BooleanField('Use Colors', validators=[
+        wtf.Optional()
+    ], default=False, description=(
+        'If checked, messages will include mIRC colouring.'
+    ))
 
 
 class PlainTextHook(HookService):
@@ -17,9 +26,16 @@ class PlainTextHook(HookService):
 
     @classmethod
     def handle_request(cls, user, request, hook):
+        config = hook.config or {}
+
         p = request.form.get('payload', None)
         if not p:
             p = request.args.get('payload', None)
             if not p:
                 return
-        yield cls.message(p[:512])
+
+        yield cls.message(p[:512], strip=not config.get('use_colours', False))
+
+    @classmethod
+    def form(cls):
+        return PlainTextConfigForm
