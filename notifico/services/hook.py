216b7dbbf801cfff86ea5c430d4eed6c5934df6e
Tyler Kennedy <tk@tkte.ch>
2022-09-23 06:19:55 -0400

    Slowly start typing core classes.

--- notifico/services/hook.py
+++ notifico/services/hook.py
@@ -1,8 +1,11 @@
 import abc
 import re
+from typing import Optional, Type
 
+import flask_wtf
 from flask import current_app
 from jinja2 import Environment, PackageLoader
+from wtforms import Form
 
 from notifico.util import irc
 from notifico.services.messages import MessageService
@@ -19,21 +22,21 @@ class HookService(abc.ABC):
     SERVICE_ID = None
 
     @classmethod
-    def description(cls):
+    def description(cls) -> str:
         """
         A description of this service as an HTML string.
         """
         return ''
 
     @staticmethod
-    def env():
+    def env() -> Environment:
         """
         Returns a Jinja2 `Environment` for template rendering.
         """
         raise NotImplementedError()
 
     @classmethod
-    def shorten(cls, url):
+    def shorten(cls, url: str) -> str:
         """
         If possible, return a shorter version of `url` shortened by a 3rd
         party service. Where the service provides its own shortening service,
@@ -42,14 +45,14 @@ class HookService(abc.ABC):
         return url
 
     @classmethod
-    def strip_colors(cls, msg):
+    def strip_colors(cls, msg: str) -> str:
         """
         Strip mIRC color codes from `msg` and return it.
         """
         return irc.strip_mirc_colors(msg)
 
     @classmethod
-    def message(cls, message, strip=True):
+    def message(cls, message: str, strip: bool = True):
         # Optionally strip mIRC color codes.
         message = cls.strip_colors(message) if strip else message
         # Strip newlines and other whitespace.
@@ -61,14 +64,14 @@ class HookService(abc.ABC):
         """
         Returns a Redis connection instance.
         """
-        return current_app.redis
+        return current_app.redis  # noqa
 
     @classmethod
     def _request(cls, user, request, hook, *args, **kwargs):
         combined = []
 
         ms = MessageService(redis=cls._redis())
-        handler = cls.handle_request(user, request, hook, *args, **kwargs)
+        handler = cls.handle_request(user, request, hook)
 
         if handler is None:
             # It's entirely possible for a message body to be a NOP,
@@ -84,7 +87,15 @@ class HookService(abc.ABC):
             ms.log_message('\n'.join(combined), hook.project)
 
     @classmethod
-    def form(cls):
+    @abc.abstractmethod
+    def handle_request(cls, user, request, hook):
+        """
+        Handle an incoming webhook.
+        """
+        raise NotImplementedError()
+
+    @classmethod
+    def form(cls) -> Optional[Type[Form]]:
         """
         Returns a wtforms.Form subclass which is a form of Service options
         to be shown to the user on setup.
@@ -92,7 +103,7 @@ class HookService(abc.ABC):
         return None
 
     @classmethod
-    def validate(cls, form, request):
+    def validate(cls, form: flask_wtf.FlaskForm, request):
         """
         Returns `True` if the form passes validation, `False` otherwise.
         Should be subclassed by complex service configurations.
@@ -100,7 +111,7 @@ class HookService(abc.ABC):
         return form.validate_on_submit()
 
     @classmethod
-    def pack_form(cls, form):
+    def pack_form(cls, form: Form) -> dict:
         """
         Returns a dictionary of configuration options processed from `form`.
         By default, simply iterates all fields, taking their ``.id`` as the
@@ -109,7 +120,7 @@ class HookService(abc.ABC):
         return dict((f.id, f.data) for f in form)
 
     @classmethod
-    def load_form(cls, form, config):
+    def load_form(cls, form: Form, config: dict) -> Optional[Form]:
         """
         Loads a Hook configuration into an existing Form object, returning it.
         """
